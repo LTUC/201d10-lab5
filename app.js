1a9def12e143001b33a869b93a8b1494925fadcc
Hamza-Rashed <hamzarashed503@gmail.com>
2020-09-10 21:14:05 +0300

    the last question

--- app.js
+++ app.js
@@ -157,7 +157,7 @@ function multiplyArray(multArr) { //eslint-disable-line
     result5.push(secondResule)
     }
     multiplyArray(testArray2)
-    console.log(result5)
+    // console.log(result5)
 
 // Here is the test for multiplyArray(); uncomment it to run it
 // testMultiplyArray(testArray);
@@ -170,22 +170,39 @@ function multiplyArray(multArr) { //eslint-disable-line
 
 /////////////////////////////////////
 /* STRETCH GOAL: Problem 6
-Write a function called multiplyAnyArray() that takes an array of numbers of any length as its argument and returns an array whose first element is the product of those numbers, and the second element is a string that EXACTLY follows this example and concatenates a message using the arguments that were passed into the function:
+Write a function called multiplyAnyArray() that takes an array of numbers of any length as
+ its argument and returns an array whose first element is the product of those numbers, and 
+ the second element is a string that EXACTLY follows this example and concatenates a message 
+ using the arguments that were passed into the function:
 
 "The numbers 1,2,3,4,5 have a product of 120."
 
-IMPORTANT DETAIL: You may not use the arithmetic operator * in this function. To do multiplication, use your multiply() function that you've already created. You're going to have to be resourceful to figure out how to do this. However, you may continue to use the + operator for string concatenation.
+IMPORTANT DETAIL: You may not use the arithmetic operator * in this function. To do 
+multiplication, use your multiply() function that you've already created. You're going 
+to have to be resourceful to figure out how to do this. However, you may continue to 
+use the + operator for string concatenation.
 
 This function should be dynamic, accepting an array of any length.
 
-Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiplyAnyArray() function and see if the test passes.*/
+Test this function by hand in the console to get it working, and when you think it is 
+finished, uncomment the call for the testMultiplyAnyArray() function and see if the test passes.*/
 
 // Write your code here
-var testDynamicArray = [1,2,3,4,5]; //eslint-disable-line
-
+let g = 3;
+let h = 4;
+let i = 2;
+let j = 4;
+let k = 2;
+var testDynamicArray = [g,h,i,j,k]; //eslint-disable-line
+let result6 = [];
 function multiplyAnyArray(dynamicArray) { //eslint-disable-line
-
-}
+    let sum = testDynamicArray.reduce((a, b) => a * b) // this is first solution
+    result6.push(sum);
+    secondResule = `The numbers ${g},${h},${i},${j},${k} have a product of ${sum}.`
+    result6.push(secondResule)
+    }
+    multiplyAnyArray(testDynamicArray)
+    console.log(result6)
 
 // Here is the test for multiplyArray(); uncomment it to run it
 // testMultiplyAnyArray(testDynamicArray);
