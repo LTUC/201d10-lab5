e15952feef9fd17713a45acfc983941345bd2339
RaniaAbdullahh <raniaabdullah6600@gmail.com>
2020-09-10 16:39:02 +0300

    problem5 done

--- app.js
+++ app.js
@@ -104,12 +104,18 @@ IMPORTANT DETAIL: You may not use the arithmetic operator * in this function. To
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiplyArray() function and see if the test passes.*/
 
 // Write your code here
-function multiplyArray(multArr) { //eslint-disable-line
+function multiplyArray(testArray) { //eslint-disable-line
+    var m=1;
+    for(var i= 0; i<testArray.length;i++){
+        m=m*testArray[i];
+    }
+    return[m,'The numbers 2,3,4 have a product of 24.']
 
 }
+console.log(multiplyArray(testArray));
 
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyArray(testArray);
+ testMultiplyArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
 
