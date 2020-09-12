298c36c77755ef62f81b5ecb9c329c1b3558e55d
farahzuot <farahz997@eng.hu.edu.jo>
2020-09-13 02:34:19 +0300

    part1 created

--- app.js
+++ app.js
@@ -8,12 +8,18 @@ Write a function called sum() that takes in two numbers as arguments and then re
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSum() function below and check the console to see if the test passes.*/
 
 // Write your code here
-function sum(a, b) { //eslint-disable-line
-
-}
+function sum() { //eslint-disable-line
+    var s = 0;
+    for (var i=0; i < arguments.length; i++) {
+        s += arguments[i];
+    }
+    var arr = [s,`The sum of ${arguments[0]} and ${arguments[1]} is ${s}.`] ;
+return arr;
+} 
+console.log(sum(4,7));
 
 // Here is the test for sum(); uncomment it to run it
-// testSum(4, 7);
+testSum(4, 7);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
