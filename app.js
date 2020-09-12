00564713050c38a7e71dd0fe40b19a747a6ad0a9
MsDiala <diala-is-diala@hotmail.com>
2020-09-12 17:02:19 +0300

    Update app.js
    problem1 task finish

--- app.js
+++ app.js
@@ -7,9 +7,11 @@ Write a function called sum() that takes in two numbers as arguments and then re
 
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSum() function below and check the console to see if the test passes.*/
 
-// Write your code here
 function sum(a, b) { //eslint-disable-line
-
+var index1 = a+b;
+var index2=a.toString()+b.toString()
+var result = [index1,index2]
+return result
 }
 
 // Here is the test for sum(); uncomment it to run it
