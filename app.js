fb37d832e9a92c1055437a342b34e77e7780446d
Alaahayek25 <alaalhayek25@gmail.com>
2020-09-13 02:55:32 +0300

    problem3

--- app.js
+++ app.js
@@ -53,9 +53,13 @@ IMPORTANT DETAIL: You may not use the arithmetic operators + and * in this funct
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSumAndMultiply() function and see if the test passes.*/
 
 // Write your code here
-function sumAndMultiply(a, b, c) { //eslint-disable-line
-
+    function sumAndMultiply(a, b, c) {
+        var Sum = sum(sum(a,b)[0],c)[0];
+        var Multuiply = multiply(multiply(a,b)[0], c)[0];
+        return([Sum, Mul, `${a} and ${b} and ${c} sum to ${Sum}.`, `The product of ${a} and ${b} and ${c} is ${Mul}.`]); 
+    
 }
+
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
 // testSumAndMultiply(4,7,5);
