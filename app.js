c4ee89a62a61fbe9a45c60b330e41ff40d128794
Alaahayek25 <alaalhayek25@gmail.com>
2020-09-13 03:17:41 +0300

    problem5

--- app.js
+++ app.js
@@ -62,7 +62,7 @@ Test this function by hand in the console to get it working, and when you think 
 
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+ testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -89,7 +89,7 @@ function sumArray(sumArr) {
 
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
+ testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -105,8 +105,14 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiplyArray(multArr) { //eslint-disable-line
-
+    var mul=1;
+    for(var i=0;i<testArray.length;i++){
+        mul=multiply(mul,testArray[i])[0];
+    }
+    return[mul,`The numbers ${testArray} have a product of ${mul}.`];
 }
+
+
 
 // Here is the test for multiplyArray(); uncomment it to run it
 // testMultiplyArray(testArray);
